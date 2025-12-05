f01fc575118abc2f8bf7a61e7e7fd85101c2c184
KK-PRO-MAX <kehaibo8@gmail.com>
2025-12-05 17:44:12 +0800

    Delete dist directory

--- dist/assets/filterStore-wW98jmOu.js
+++
@@ -1,1 +0,0 @@
-import{Y as r,r as o,x as a}from"./index-COfldkwK.js";const n=r("filter",()=>{const e=o("");return a(e,t=>{console.log("Global filter changed:",t)}),{selectedCategory:e,setCategory:t=>{e.value=t},clearFilter:()=>{e.value=""}}});export{n as u};
